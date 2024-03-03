98d4ac0692ca0a7589d5cc869c75ef4b220b3beb
James Wood <woodjames275@gmail.com>
2024-03-03 16:39:44 -0700

    Initial Commit

---
+++ Health and Wellness/Health_and_WellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_and_WellnessApp.swift
+//  Health and Wellness
+//
+//  Created by James Wood on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Health_and_WellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
